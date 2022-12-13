da7b99ca01f2bd3175ab7e285cb1ee3b796684b9
Shiv Archit <shiv.archit@patientbond.com>
2022-12-13 15:19:49 +0530

    Fixed index.js to act as starting point for worker callback

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const fs = require("fs");
+const Cors = require("cors");
 const path = require('path');
 const twilio = require('twilio');
+// const response = new twilio.Response();
+// response.appendHeader('Access-Control-Allow-Origin', '*');
+// response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+// response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
 const BodyParser = require('body-parser');
 const AccessToken = twilio.jwt.AccessToken;
 const VoiceResponse = twilio.twiml.VoiceResponse;
@@ -13,13 +19,18 @@ const Policy = TaskRouterCapability.Policy;
 const VoiceGrant = AccessToken.VoiceGrant;
 const TASKROUTER_BASE_URL = 'https://taskrouter.twilio.com';
 const version = 'v1';
-const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const app = express();
-
+var router = express.Router();
+app.use(Cors({
+    origin: "*",
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 const activities = {
-    "Offline": "WA520590de6681ddef0f370997a0562f9b",
-    "Available": "WA1b2f786cf7dff20fea4e23af1e08b8df",
-    "Unavailable": "WA681977fa391bd637a13d3ae805461d19"
+    "Offline": "WA352e959e389ac80c057748bc8661e7c5",
+    "Available": "WAf7e118ae4296aa4c958fffd65d51e462",
+    "Unavailable": "WAa912cd188d5486f3bd5dd391c8cb69f0",
+    "OnCall": "WA3059b94e426e71b98f2b49e00c29ab6e"
 };
 
 app.use(BodyParser.json({
@@ -29,6 +40,52 @@ app.use(BodyParser.urlencoded({
     extended: false
 }));
 
+app.use(router);
+router.post('/incoming/', function (req, res) {
+    // console.log(JSON.stringify(req.body));
+
+    fs.appendFileSync('log.txt', `\n STARTING CALL LOG FOR ${req.body.CallSid} \n`);
+    fs.appendFileSync('log.txt', JSON.stringify({ "callData": req.body }));
+    fs.appendFileSync('log.txt', "\n");
+    var twimlResponse = new VoiceResponse();
+    var gather = twimlResponse.gather({
+        numDigits: 1,
+        action: '/enqueue/',
+        method: 'POST'
+    });
+    gather.say('For Support, press one. For Payment, press any other key.');
+    res.type('text/xml');
+    // console.log(twimlResponse.toString());
+    res.send(twimlResponse.toString());
+});
+
+// POST /call/enqueue
+router.post('/enqueue/', function (req, res) {
+    // console.log(JSON.stringify(req.body));
+    fs.appendFileSync('log.txt', `Digit Pressed ${req.body.Digits}\n`);
+    var pressedKey = req.body.Digits;
+    var twimlResponse = new VoiceResponse();
+    var selectedProduct = (pressedKey === '1') ? 'support' : 'payment';
+    var enqueue = twimlResponse.enqueue(
+        { workflowSid: (pressedKey === '1') ? process.env.WORK_FLOW_ID : "WW1105a486b5cf6d2fc86f79430ce3f348" }
+    );
+    fs.appendFileSync('log.txt', `Assiging to workFlowTask ${process.env.WORK_FLOW_ID}\n`);
+    enqueue.task({
+        priority: (pressedKey === '1') ? 5 : 1,
+        timeout: 500
+    }, JSON.stringify({ selected_product: selectedProduct }));
+
+    res.setHeader('Content-Type', 'application/xml');
+    res.write(enqueue.toString());
+    res.end();
+    // res.type('text/xml');
+    // res.send(twimlResponse.toString());
+});
+router.post("/allCallBacks", function (req, res) {
+    fs.appendFileSync('callback.txt', `Callback ${JSON.stringify(req.body)}\n`);
+    // console.log((req.body));
+    res.status(200);
+});
 app.post('/voice', twilio.webhook({ validate: false }), function (req, res) {
     var phoneNumber = req.body.phoneNumber;
     var callerId = process.env.TWILIO_NUMBER;
@@ -67,7 +124,7 @@ app.get('/worker', (req, res) => {
         })
         .then(workers => {
             if (workers && workers[0]) {
-                res.status(200).json({workerId: workers[0].sid});
+                res.status(200).json({ workerId: workers[0].sid });
             }
 
             res.status(500).end();
@@ -120,6 +177,9 @@ app.post("/worker-token", (req, res) => {
         buildWorkspacePolicy({ resources: ['Activities'], method: 'POST' }),
         // Workspace Activities Worker Reserations Policy
         buildWorkspacePolicy({ resources: ['Workers', req.body.workerId, 'Reservations', '**'], method: 'POST' }),
+        buildWorkspacePolicy({ resources: ['Tasks', '*', 'Reservations', "*"], method: 'POST' })
+
+
     ];
 
     eventBridgePolicies.concat(workerPolicies).concat(workspacePolicies).forEach(function (policy) {
@@ -138,8 +198,8 @@ app.post("/worker-status", (req, res) => {
             activitySid: activities[req.body.activityName]
         })
         .then(worker => res.status(200).end())
-        .catch(ex => res.status(500).end());
-})
+        .catch(ex => { console.log(ex); res.status(500).end(); });
+});
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
