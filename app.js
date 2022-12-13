eb2ca473be65b145c5adc8a86cc8449fb034944c
Shiv Archit <shiv.archit@patientbond.com>
2022-12-13 13:11:29 +0530

    Added assingment callback and logging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const BodyParser = require("body-parser");
 const port = 3000;
@@ -17,6 +18,11 @@ app.use(BodyParser.urlencoded({
 
 // POST /call/incoming
 router.post('/incoming/', function (req, res) {
+    // console.log(JSON.stringify(req.body));
+
+    fs.appendFileSync('log.txt', `\n STARTING CALL LOG FOR ${req.body.CallSid} \n`);
+    fs.appendFileSync('log.txt', JSON.stringify({ "callData": req.body }));
+    fs.appendFileSync('log.txt', "\n");
     var twimlResponse = new VoiceResponse();
     var gather = twimlResponse.gather({
         numDigits: 1,
@@ -25,27 +31,37 @@ router.post('/incoming/', function (req, res) {
     });
     gather.say('For Normal Support, press one. For Payment Support, press any other key.');
     res.type('text/xml');
-    console.log(twimlResponse.toString());
+    // console.log(twimlResponse.toString());
     res.send(twimlResponse.toString());
 });
 
 // POST /call/enqueue
 router.post('/enqueue/', function (req, res) {
+    // console.log(JSON.stringify(req.body));
+    fs.appendFileSync('log.txt', `Digit Pressed ${req.body.Digits}\n`);
     var pressedKey = req.body.Digits;
     var twimlResponse = new VoiceResponse();
     var selectedProduct = (pressedKey === '1') ? 'support' : 'payment';
     var enqueue = twimlResponse.enqueue(
-        { workflowSid: (pressedKey === '1') ? "WW691193cb80241adce46cfac5463e05b0" : "WW1105a486b5cf6d2fc86f79430ce3f348" }
+        { workflowSid: (pressedKey === '1') ? process.env.WORK_FLOW_ID : "WW1105a486b5cf6d2fc86f79430ce3f348" }
     );
+    fs.appendFileSync('log.txt', `Assiging to workFlowTask ${process.env.WORK_FLOW_ID}\n`);
     enqueue.task({
-        priority: 5,
+        priority: (pressedKey === '1') ? 5 : 1,
         timeout: 500
     }, JSON.stringify({ selected_product: selectedProduct }));
+
     res.setHeader('Content-Type', 'application/xml');
-    res.write(enqueue.toString());
+    res.write(enqueue.toString());    
     res.end();
     // res.type('text/xml');
     // res.send(twimlResponse.toString());
+});
+
+router.post("/holdCall", function (req, res) {
+    fs.appendFileSync('log.txt', `Holding Call with assigned worker ${JSON.stringify(req.body)}\n`);
+    console.log((req.body));
+    res.status(200);
 });
 
 router.get("/taskQueues", function (req, res) {
@@ -99,10 +115,9 @@ router.get("/tasks", function (req, res) {
 router.get("/getFlow/:flowId", function (req, res) {
 
     client.studio.v2
-        .flows(req.params.flowId)      
-        .fetch()  
+        .flows(req.params.flowId)
+        .fetch()
         .then(flows => res.status(200).json({ flows: flows }));
-    // https://studio.twilio.com/v2/Flows
 });
 router.get("/getFlows", function (req, res) {
 
@@ -117,6 +132,8 @@ router.get("/getFlows", function (req, res) {
 });
 
 
+
+
 // POST /call/assignment
 router.post('/assignment/', function (req, res) {
     res.type('application/json');
@@ -124,6 +141,12 @@ router.post('/assignment/', function (req, res) {
         instruction: "dequeue",
         post_work_activity_sid: app.get('workspaceInfo').activities.idle
     });
+});
+
+router.post("/callReceived", function (req, res) {
+
+    console.log(JSON.stringify(req.body));
+    res.sendStatus(200);
 });
 
 app.use(router);
