4c2a0fdbed315dc963b2b79ea3375ba9be7dc5ac
Chaitanya Kumar <chaitanya.kumar@patientbond.com>
2022-12-13 13:29:22 +0530

    added worker code for ui

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const twilio = require('twilio');
 const BodyParser = require('body-parser');
-const AccessToken = require('twilio').jwt.AccessToken;
+const AccessToken = twilio.jwt.AccessToken;
 const VoiceResponse = twilio.twiml.VoiceResponse;
+const taskrouter = twilio.jwt.taskrouter;
+const util = taskrouter.util;
+const TaskRouterCapability = taskrouter.TaskRouterCapability;
+const Policy = TaskRouterCapability.Policy;
 
 const VoiceGrant = AccessToken.VoiceGrant;
+const TASKROUTER_BASE_URL = 'https://taskrouter.twilio.com';
+const version = 'v1';
+const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 const app = express();
+
+const activities = {
+    "Offline": "WA520590de6681ddef0f370997a0562f9b",
+    "Available": "WA1b2f786cf7dff20fea4e23af1e08b8df",
+    "Unavailable": "WA681977fa391bd637a13d3ae805461d19"
+};
 
 app.use(BodyParser.json({
     extended: false
@@ -17,7 +31,7 @@ app.use(BodyParser.urlencoded({
 
 app.post('/voice', twilio.webhook({ validate: false }), function (req, res) {
     var phoneNumber = req.body.phoneNumber;
-    var callerId = '+13867031616';
+    var callerId = process.env.TWILIO_NUMBER;
     var twiml = new VoiceResponse();
 
     var dial = twiml.dial({ callerId: callerId });
@@ -30,10 +44,10 @@ app.post('/voice', twilio.webhook({ validate: false }), function (req, res) {
     res.send(twiml.toString());
 });
 
-app.get('/token', (req, res) => {
-    const clientName = "support"
+app.post('/token', (req, res) => {
+    const clientName = req.body.clientName;
 
-    const accessToken = new AccessToken('AC79d92e81368d553caea3d593e63d9c30', 'SK76976b19f563564fb28c5ca700c6e66f', 'IF2xL28X8bpfjX5BdESQPAPWNX3K40BT');
+    const accessToken = new AccessToken(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_API_KEY, process.env.TWILIO_API_SECRET);
     accessToken.identity = clientName;
 
     const grant = new VoiceGrant({
@@ -45,16 +59,110 @@ app.get('/token', (req, res) => {
     res.status(200).json({ token: accessToken.toJwt() });
 });
 
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/worker', (req, res) => {
+    twilioClient.taskrouter.v1.workspaces(process.env.TWILIO_WORKSPACE_ID)
+        .workers
+        .list({
+            targetWorkersExpression: `friendly_name IN ['${req.query.name}']`
+        })
+        .then(workers => {
+            if (workers && workers[0]) {
+                res.status(200).json({workerId: workers[0].sid});
+            }
+
+            res.status(500).end();
+        })
+        .catch(ex => {
+            res.status(500).end();
+        })
 });
 
-app.get('/:filename', (req, res) => {
+app.post('/worker', (req, res) => {
+    let attributes = {
+        "contact_uri": `client:${req.body.workerName}`
+    }
+
+    twilioClient.taskrouter.v1.workspaces(process.env.TWILIO_WORKSPACE_ID)
+        .workers
+        .create({
+            friendlyName: req.body.workerName,
+            attributes: JSON.stringify(attributes)
+        })
+        .then((worker) => {
+            res.status(201).json({
+                workerId: worker.sid
+            });
+        })
+        .catch((ex) => {
+            res.status(500).json(ex);
+        })
+});
+
+app.post("/worker-token", (req, res) => {
+    const capability = new TaskRouterCapability({
+        accountSid: process.env.TWILIO_ACCOUNT_SID,
+        authToken: process.env.TWILIO_AUTH_TOKEN,
+        workspaceSid: process.env.TWILIO_WORKSPACE_ID,
+        channelId: req.body.workerId
+    });
+
+    const eventBridgePolicies = util.defaultEventBridgePolicies(process.env.TWILIO_ACCOUNT_SID, req.body.workerId);
+
+    // Worker Policies
+    const workerPolicies = util.defaultWorkerPolicies(version, process.env.TWILIO_WORKSPACE_ID, req.body.workerId);
+
+    const workspacePolicies = [
+        // Workspace fetch Policy
+        buildWorkspacePolicy(),
+        // Workspace subresources fetch Policy
+        buildWorkspacePolicy({ resources: ['**'] }),
+        // Workspace Activities Update Policy
+        buildWorkspacePolicy({ resources: ['Activities'], method: 'POST' }),
+        // Workspace Activities Worker Reserations Policy
+        buildWorkspacePolicy({ resources: ['Workers', req.body.workerId, 'Reservations', '**'], method: 'POST' }),
+    ];
+
+    eventBridgePolicies.concat(workerPolicies).concat(workspacePolicies).forEach(function (policy) {
+        capability.addPolicy(policy);
+    });
+
+    const token = capability.toJwt();
+
+    res.status(201).json({ token });
+});
+
+app.post("/worker-status", (req, res) => {
+    twilioClient.taskrouter.v1.workspaces(process.env.TWILIO_WORKSPACE_ID)
+        .workers(req.body.workerId)
+        .update({
+            activitySid: activities[req.body.activityName]
+        })
+        .then(worker => res.status(200).end())
+        .catch(ex => res.status(500).end());
+})
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(path.join(__dirname, '/worker/worker.html'));
+});
+
+app.get('/js/:filename', (req, res) => {
     res.setHeader('Content-Type', 'application/javascript');
-    res.sendFile(path.join(__dirname, `/${req.params.filename}`));
+    res.sendFile(path.join(__dirname, `/js/${req.params.filename}`));
 });
 
 app.listen(3000, () => {
     console.log('Listening on 3000');
 });
+
+function buildWorkspacePolicy(options) {
+    options = options || {};
+    var resources = options.resources || [];
+    var urlComponents = [TASKROUTER_BASE_URL, version, 'Workspaces', process.env.TWILIO_WORKSPACE_ID]
+
+    return new Policy({
+        url: urlComponents.concat(resources).join('/'),
+        method: options.method || 'GET',
+        allow: true
+    });
+};
---
+++ js/worker.js
@@ -0,0 +1,169 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const createBtn = document.getElementById("create");
+    const onlineBtn = document.getElementById("online");
+    const offlineBtn = document.getElementById("offline");
+    const workerTokenBtn = document.getElementById("worker-token");
+    const getWorkerBtn = document.getElementById("get-worker");
+    const statusHeader = document.getElementById("status");
+    const currentWorker = document.getElementById("current");
+
+    const twilioResourceDetails = {
+        device: undefined,
+        worker: undefined,
+        workerId: undefined,
+        clientName: undefined
+    };
+
+    getWorkerBtn.addEventListener("click", () => {
+        let workerName = document.getElementById("worker-name");
+
+        if (workerName && workerName.value) {
+            fetch(`/worker?name=${workerName.value}`)
+                .then(res => res.json())
+                .then(data => {
+                    twilioResourceDetails.workerId = data.workerId;
+                    twilioResourceDetails.clientName = workerName.value;
+
+                    currentWorker.innerText = `Current Worker: ${twilioResourceDetails.clientName}`;
+                })
+                .catch(ex => console.log(ex));
+        }
+    });
+
+    createBtn.addEventListener("click", () => {
+        let workerName = document.getElementById("worker-name");
+
+        if (workerName && workerName.value) {
+            fetch("/worker", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ workerName: workerName.value })
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    twilioResourceDetails.workerId = data.workerId;
+                    twilioResourceDetails.clientName = workerName.value;
+
+                    currentWorker.innerText = `Current Worker: ${twilioResourceDetails.clientName}`;
+                })
+                .catch((ex) => console.log(ex));
+        }
+    });
+
+    workerTokenBtn.addEventListener("click", () => {
+        if (twilioResourceDetails && twilioResourceDetails.workerId && twilioResourceDetails.clientName) {
+            fetch("/worker-token", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ workerId: twilioResourceDetails.workerId })
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data && data.token) {
+                        const worker = new Twilio.TaskRouter.Worker(data.token);
+                        twilioResourceDetails.worker = worker;
+                        registerWorkerCallbacks(worker);
+                    }
+                })
+                .catch((ex) => console.log(ex));
+
+            fetch("/token", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ clientName: twilioResourceDetails.clientName })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data && data.token) {
+                        const device = new Twilio.Device(data.token, {
+                            // Set Opus as our preferred codec. Opus generally performs better, requiring less bandwidth and
+                            // providing better audio quality in restrained network conditions. Opus will be default in 2.0.
+                            codecPreferences: ["opus", "pcmu"],
+                            // Use fake DTMF tones client-side. Real tones are still sent to the other end of the call,
+                            // but the client-side DTMF tones are fake. This prevents the local mic capturing the DTMF tone
+                            // a second time and sending the tone twice. This will be default in 2.0.
+                            fakeLocalDTMF: true,
+                            // Use `enableRingingState` to enable the device to emit the `ringing`
+                            // state. The TwiML backend also needs to have the attribute
+                            // `answerOnBridge` also set to true in the `Dial` verb. This option
+                            // changes the behavior of the SDK to consider a call `ringing` starting
+                            // from the connection to the TwiML backend to when the recipient of
+                            // the `Dial` verb answers.
+                            enableRingingState: true
+                        });
+                        twilioResourceDetails.device = device;
+                        device.register();
+                        registerDeviceCallbacks(device);
+                    }
+                })
+        }
+    });
+
+    onlineBtn.addEventListener("click", () => {
+        if (twilioResourceDetails.worker && twilioResourceDetails.workerId) {
+            fetch("/worker-status", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ workerId: twilioResourceDetails.workerId, activityName: "Available" })
+            })
+                .catch((ex) => console.log(ex));
+        }
+    });
+
+    offlineBtn.addEventListener("click", () => {
+        if (twilioResourceDetails.worker && twilioResourceDetails.workerId) {
+            fetch("/worker-status", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ workerId: twilioResourceDetails.workerId, activityName: "Offline" })
+            })
+                .catch((ex) => console.log(ex));
+        }
+    });
+
+    function registerDeviceCallbacks(device) {
+        device.on('registered', device => {
+            console.log("Device ready to receive calls");
+        });
+
+        device.on("incoming", function (conn) {
+            console.log("Incoming call from ", conn);
+            // // Set a callback to be executed when the connection is accepted
+            conn.accept(function () {
+                console.log('Call connected');
+            });
+
+            // // Set a callback on the answer button and enable it
+            // answerButton.click(function () {
+            //     conn.accept();
+            // });
+            // answerButton.prop("disabled", false);
+        });
+    };
+
+    function registerWorkerCallbacks(worker) {
+        worker.on("ready", (worker) => {
+            console.log(worker);
+        });
+
+        worker.on("activity.update", (worker) => {
+            console.log(worker.activityName)
+            statusHeader.innerText = `Current Status: ${worker.activityName}`;
+        });
+
+        worker.on("reservation.created", (reservation) => {
+            reservation.dequeue();
+        })
+    };
+});
